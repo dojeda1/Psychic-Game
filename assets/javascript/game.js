e9f9658241195eee25647880889a3890cce207e2
Dominic Ojeda <dominicojeda.coding@gmail.com>
2019-02-18 13:23:12 -0700

    Basic game works. Now need to make computer guess a callable function.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,21 +1,22 @@
-// document.onkeyup = function (event) {
+var wins = 0;
+var losses = 0;
+var guessesLeft = 10;
 
-//     var keyPress = event.key.toLowerCase();
-
-//     if (keyPress === "1") {
-
-
-//         console.log(keyPress)
-//     } else {
-//         console.log("invalid")
-//     }
-// }
+document.getElementById("winCount").innerHTML = wins;
+document.getElementById("lossCount").innerHTML = losses;
+document.getElementById("guessCount").innerHTML = guessesLeft;
 
 
+
+
+
+// computer chooses random letter
 
 
 function compNum() {
     return Math.floor(Math.random() * 26) + 65;
+
+
 }
 
 var compLetter = String.fromCharCode(compNum());
@@ -25,8 +26,10 @@ var compFinal = compLetter.toLowerCase();
 console.log(compLetter);
 console.log(compFinal);
 
+
 document.addEventListener("keyup", checkKeyPress);
 
+// only alphabetic keys register
 
 function checkKeyPress(key) {
 
@@ -37,9 +40,30 @@ function checkKeyPress(key) {
         console.log("Player Guess: " + keyPress);
 
         if (keyPress === compFinal) {
-            console.log("the same")
+            console.log("the same");
+            wins++;
+            document.getElementById("winCount").innerHTML = wins;
+            guessesLeft = 10;
+            document.getElementById("guessCount").innerHTML = guessesLeft;
+            document.getElementById("guessList").innerHTML = "";
+
         } else {
-            console.log("nope")
+
+            if (guessesLeft > 1) {
+                console.log("nope");
+                guessesLeft--;
+                document.getElementById("guessCount").innerHTML = guessesLeft;
+                document.getElementById("guessList").innerHTML = document.getElementById("guessList").innerHTML + keyPress + " ";
+
+            } else {
+                console.log("you lose")
+                guessesLeft = 10;
+                losses++;
+                document.getElementById("lossCount").innerHTML = losses;
+                document.getElementById("guessCount").innerHTML = guessesLeft;
+                document.getElementById("guessList").innerHTML = "";
+
+            }
         }
 
     } else {
